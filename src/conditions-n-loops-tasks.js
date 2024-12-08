f6f174a3633da625d527f68dc73916402012205a
Aleksandr <tsybulskialex@gmail.com>
2024-12-08 15:35:17 +0300

    feat: completed 12 tasks

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -360,8 +360,21 @@ function rotateMatrix(/* matrix */) {
  *  [2, 9, 5, 9]    => [2, 5, 9, 9]
  *  [-2, 9, 5, -3]  => [-3, -2, 5, 9]
  */
-function sortByAsc(/* arr */) {
-  throw new Error('Not implemented');
+function sortByAsc(arr) {
+  const newArr = arr;
+  for (let i = 0; i < newArr.length; i += 1) {
+    let cur = newArr.length - 1 - i;
+    for (let j = 0; j < newArr.length - i; j += 1) {
+      if (newArr[j] > newArr[cur]) {
+        cur = j;
+      }
+    }
+    [newArr[newArr.length - 1 - i], newArr[cur]] = [
+      newArr[cur],
+      newArr[newArr.length - 1 - i],
+    ];
+  }
+  return newArr;
 }
 
 /**
